689a70465316faedc0af946332159a2055d712e5
ej522 <jsej07@gmail.com>
2023-02-23 23:01:31 +0900

    Initial commit

---
+++ src/main/java/com/example/beside/BesideApplication.java
@@ -0,0 +1,13 @@
+package com.example.beside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beside/BesideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BesideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
