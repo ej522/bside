aa485f69040b1a44734f888c68a028c910dc5a33
jinsujj <wlstncjs1234@naver.com>
2023-02-27 15:01:25 +0900

    swagger 설정 수정

--- src/main/java/com/example/beside/controller/WelcomeController.java
+++ src/main/java/com/example/beside/controller/WelcomeController.java
@@ -2,18 +2,12 @@ package com.example.beside.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class WelcomeController {
 
     @GetMapping("/")
-    @ResponseBody
-    public RedirectView hello() {
-        RedirectView redirectView = new RedirectView();
-        redirectView.setUrl("http://localhost/swagger-ui/index.html");
-
-        return redirectView;
+    public String hello() {
+        return "redirect:/swagger-ui/index.html";
     }
 }
