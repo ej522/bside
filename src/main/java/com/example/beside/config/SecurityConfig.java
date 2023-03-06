01edf5995ba704004b66d35f9620489cebbbf19a
ej522 <jsej07@gmail.com>
2023-03-07 00:13:16 +0900

    spring security + jwt 적용중

---
+++ src/main/java/com/example/beside/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.beside.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().
+                authorizeHttpRequests().requestMatchers("/swagger/**","/swagger-ui/**","/swagger-resources/**","/webjars/**","/v2/api-docs","/login").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
