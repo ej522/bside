f0cc91b1a4a5d30ae7e950a831f5332e979e1897
ej522 <jsej07@gmail.com>
2023-03-22 00:43:45 +0900

    카카오로그인 수정, 테스트코드 추가

--- src/main/java/com/example/beside/api/user/SocialLoginController.java
+++ src/main/java/com/example/beside/api/user/SocialLoginController.java
@@ -1,15 +1,11 @@
 package com.example.beside.api.user;
 
-import com.example.beside.domain.User;
 import com.example.beside.service.SocialLoginService;
 import com.example.beside.service.UserService;
 import com.example.beside.util.JwtProvider;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,17 +25,11 @@ public class SocialLoginController {
 
 
     @GetMapping(value = "/v1/kakaoLogin")
-    public ResponseEntity kakaoLogin(@RequestParam("code") String code, HttpServletResponse response) {
-        HashMap<String, Object> userInfo = socialLoginService.getUserInfo(code);
+    public String kakaoLogin(@RequestParam("code") String code, HttpServletResponse response) {
+        HashMap<String, Object> userInfo = socialLoginService.getKaKaoUserInfo(code);
 
-        User user = (User) userInfo.get("user");
+        //jwt 토큰발급 추가예정
 
-        //jwt 토큰발급
-        String jwtToken = jwtProvider.createToken(user);
-
-        // JWT 토큰 헤더에 담아 전달
-        response.addHeader("Authorization","Bearer "+jwtToken);
-
-        return new ResponseEntity(HttpStatus.OK);
+        return userInfo.get("kakaoAccount").toString();
     }
 }
---
+++ src/test/java/com/example/beside/service/SocialLoginServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.beside.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SocialLoginServiceTest {
+
+    @Test
+    @DisplayName("카카오유저정보")
+    void getKakaoUserInfo() {
+        //given
+        //카카오에서 받은 토큰
+        String token = "";
+
+        //when
+        HashMap<String, Object> userInfo = SocialLoginService.getKaKaoUserInfo(token);
+        String kakaoAcountInfo = userInfo.get("kakaoAccount").toString();
+
+        //then
+        assertTrue(kakaoAcountInfo.length()>1);
+
+    }
+
+}
