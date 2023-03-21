73b0cd969d77d1c46ebd2b55258529d91df598a1
ej522 <75981333+ej522@users.noreply.github.com>
2023-03-21 21:50:12 +0900

    Feature/kakaologin (#7)
    * 카카오로그인구현, JWT적용x
    
    * 카카오로그인
    
    * jaxb제거
    
    * 카카오로그인수정
    
    * 주석제거
    
    ---------
    
    Co-authored-by: MinEunJi <eunji@MinEunJiui-MacBookPro.local>

---
+++ src/main/java/com/example/beside/api/user/SocialLoginController.java
@@ -0,0 +1,45 @@
+package com.example.beside.api.user;
+
+import com.example.beside.domain.User;
+import com.example.beside.service.SocialLoginService;
+import com.example.beside.service.UserService;
+import com.example.beside.util.JwtProvider;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class SocialLoginController {
+
+    private final SocialLoginService socialLoginService;
+    private final UserService userService;
+    private final JwtProvider jwtProvider;
+
+
+    @GetMapping(value = "/v1/kakaoLogin")
+    public ResponseEntity kakaoLogin(@RequestParam("code") String code, HttpServletResponse response) {
+        HashMap<String, Object> userInfo = socialLoginService.getUserInfo(code);
+
+        User user = (User) userInfo.get("user");
+
+        //jwt 토큰발급
+        String jwtToken = jwtProvider.createToken(user);
+
+        // JWT 토큰 헤더에 담아 전달
+        response.addHeader("Authorization","Bearer "+jwtToken);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
