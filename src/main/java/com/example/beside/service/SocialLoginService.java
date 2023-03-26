9819c9fe765a9669a8e7d9c4f73b1a4112d83e52
ej522 <75981333+ej522@users.noreply.github.com>
2023-03-26 17:01:40 +0900

    Feature/kakaologin (#8)
    * 카카오로그인구현, JWT적용x
    
    * 카카오로그인
    
    * jaxb제거
    
    * 카카오로그인수정
    
    * 주석제거
    
    * 카카오로그인 수정, 테스트코드 추가
    
    * 카카오로그인 수정, 테스트코드 추가
    
    * error 수정
    
    * 카카오로그인 테스트코드 결과 주석처리
    
    ---------
    
    Co-authored-by: MinEunJi <eunji@MinEunJiui-MacBookPro.local>
    Co-authored-by: Jinsu <wlstncjs1234@naver.com>

--- src/main/java/com/example/beside/service/SocialLoginService.java
+++ src/main/java/com/example/beside/service/SocialLoginService.java
@@ -1,16 +1,12 @@
 package com.example.beside.service;
 
-import com.example.beside.domain.User;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
@@ -20,62 +16,8 @@ public class SocialLoginService {
     @Autowired
     private UserService userService;
 
-    //카카오 accesstoken 발급
-    public String getKakaoAccessToken (String code) {
-        String access_token = "";
-        String refresh_token = "";
-        String reqUrl = "https://kauth.kakao.com/oauth/token";
-
-        try {
-            URL url = new URL(reqUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            //POST 요청을 위해 기본 값이 false인 setDoOutput을 true로
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-
-            //POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=authorization_code");
-            sb.append("&client_id=e0e9bb8073cb2394e6b373ed610a0635");
-            sb.append("redirect_uri=http://localhost:8081/v1/kakaoLogin");
-            sb.append("&code=" + code);
-            bw.write(sb.toString());
-            bw.flush();
-
-            //결과 코드가 200이라면 성공
-            int responseCode = conn.getResponseCode();
-            System.out.printf("responseCode="+responseCode);
-
-            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
-
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-
-            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-
-            access_token = element.getAsJsonObject().get("access_token").getAsString();
-            refresh_token = element.getAsJsonObject().get("refresh_token").getAsString();
-
-            br.close();
-            bw.close();
-
-        } catch (Exception e) {
-            e.getMessage();
-        }
-
-        return access_token;
-    }
-
-    //카카오 유저 정보 얻기
-    public HashMap<String, Object> getUserInfo (String access_token) {
+    // 카카오 유저 정보 얻기
+    public static HashMap<String, Object> getKaKaoUserInfo(String access_token) {
         HashMap<String, Object> userInfo = new HashMap<>();
         String reqUrl = "https://kapi.kakao.com/v2/user/me";
 
@@ -84,11 +26,10 @@ public class SocialLoginService {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
 
-            //요청에 필요한 Header에 포함될 내용
+            // 요청에 필요한 Header에 포함될 내용
             conn.setRequestProperty("Authorization", "Bearer " + access_token);
 
             int responseCode = conn.getResponseCode();
-            System.out.printf("responseCode: " + responseCode);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
@@ -98,27 +39,19 @@ public class SocialLoginService {
             while ((line = br.readLine()) != null) {
                 result += line;
             }
-            System.out.printf("response body: " + result);
 
             JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
+            JsonObject obj = (JsonObject) parser.parse(result);
 
-            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
-            JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+            JsonObject kakao_account = (JsonObject) obj.get("kakao_account");
+            JsonObject properties = (JsonObject) obj.get("properties");
 
-            String email = kakao_account.getAsJsonObject().get("email").getAsString();
+            userInfo.put("kakaoAccount", kakao_account);
 
-            User user = new User();
-            user.setEmail(email);
+            // 정보받아와서 db등록 여부 확인 후 DB추가할 곳
 
-            //이메일 없으면 회원가입
-            if(userService.findUserByEmail(email)==null) {
-                userService.saveUser(user);
-            }
-
-            userInfo.put("user", user);
         } catch (Exception e) {
-            e.getMessage();
+            System.out.println(e.getMessage());
         }
 
         return userInfo;
---
+++ src/test/java/com/example/beside/service/SocialLoginServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.beside.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SocialLoginServiceTest {
+
+    @Test
+    @DisplayName("카카오유저정보")
+    void getKakaoUserInfo() {
+        //given
+        String token = "카카오 access token";
+
+        //when
+        HashMap<String, Object> userInfo = SocialLoginService.getKaKaoUserInfo(token);
+        String kakaoAcountInfo = "";
+        if (userInfo.get("kakaoAccount")!=null)
+        {
+            kakaoAcountInfo = userInfo.get("kakaoAccount").toString();
+        }
+
+        //then
+        //assertTrue(kakaoAcountInfo.length()>1);
+
+    }
+
+}
