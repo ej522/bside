3045d85b1e0619a05226ef9272af88bc6b9c4021
Jinsu <wlstncjs1234@naver.com>
2023-03-29 23:49:45 +0900

    db erd 설계 (#12)

---
+++ src/main/java/com/example/beside/domain/Moim.java
@@ -0,0 +1,41 @@
+package com.example.beside.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "moim", schema = "bside")
+public class Moim {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "moim_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(length = 20)
+    private String moim_name;
+
+    private String[] selected_date;
+
+    @Column(length = 15)
+    private String fixed_date;
+
+    @Column(length = 15)
+    private String fixed_time;
+
+    private int dead_line_hour;
+
+    private LocalDateTime created_time;
+
+}
--- src/main/java/com/example/beside/domain/User.java
+++ src/main/java/com/example/beside/domain/User.java
@@ -1,9 +1,12 @@
 package com.example.beside.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.beside.util.PasswordConverter;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
@@ -16,8 +19,23 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Moim> moim = new ArrayList();
+
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Friend friend;
+
+    @Column(length = 10)
+    private String social_type;
+
+    @Column(length = 10)
+    private String name;
+
+    @Column(length = 30)
+    private String email;
+
     @Convert(converter = PasswordConverter.class)
     private String password;
 
-    private String email;
+    private String profile_image;
 }
