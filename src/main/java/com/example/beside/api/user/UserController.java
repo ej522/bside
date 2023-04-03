80531534de3711ca7d5cd0a607812e943f256d78
jinsujj <wlstncjs1234@naver.com>
2023-04-03 21:35:06 +0900

    Swagger 기능 추가합니다

--- src/main/java/com/example/beside/api/user/UserController.java
+++ src/main/java/com/example/beside/api/user/UserController.java
@@ -1,53 +1,69 @@
 package com.example.beside.api.user;
 
-
 import com.example.beside.domain.User;
 import com.example.beside.dto.UserDto;
 import com.example.beside.service.UserService;
-import com.example.beside.util.JwtProvider;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-@RestController
+@Tag(name = "User", description = "유저 API")
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/users")
+@RestController
 public class UserController {
 
     private final UserService userService;
-    private final JwtProvider jwtProvider;
 
-    @GetMapping(value ="/v1/users")
-    public List<UserDto> getAllUsers(){
-        List<UserDto>UserDtoList = new ArrayList<>();
+    @Operation(tags = { "User" }, summary = "사용자 목록 페이지 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "사용자 목록 조회를 성공했습니다."),
+            @ApiResponse(responseCode = "400", description = "존재하지 않는 사용자입니다.")
+    })
+    @GetMapping(value = "/v1/users")
+    public List<UserDto> getAllUsers() {
+        List<UserDto> UserDtoList = new ArrayList<>();
 
         List<User> userAll = userService.findUserAll();
-        userAll.forEach( s -> {
+        userAll.forEach(s -> {
             UserDto userDto = new UserDto(s.getId(), s.getPassword(), s.getEmail());
             UserDtoList.add(userDto);
         });
         return UserDtoList;
     }
 
-    @PostMapping(value = "/v1/users")
+    @Operation(tags = { "User" }, summary = "회원가입")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "회원가입이 완료되었습니다."),
+            @ApiResponse(responseCode = "400", description = "올바른 형식의 이메일 주소여야 합니다")
+    })
+    @PostMapping(value = "/v1/signup")
     public void createUser(@RequestBody @Validated CreateUserRequest requset) {
         User user = new User();
         user.setEmail(requset.email);
         user.setPassword(requset.password);
-        
+
         userService.saveUser(user);
     }
 
-    @DeleteMapping(value ="/v1/users")
-    public void deleteUser(@RequestBody @Validated DeleteUserRequest request){
+    @Operation(tags = { "User" }, summary = "회원삭제")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저가 삭제되었습니다."),
+            @ApiResponse(responseCode = "400", description = "존재하지 않는 유저입니다")
+    })
+    @DeleteMapping(value = "/v1/delete")
+    public void deleteUser(@RequestBody @Validated DeleteUserRequest request) {
 
         User user = new User();
         user.setEmail(request.email);
@@ -55,24 +71,19 @@ public class UserController {
         userService.deleteUser(user);
     }
 
-    @PostMapping("/v1/createToken")
-    public ResponseEntity<?> createToken(@RequestBody User request) {
-        String token = jwtProvider.createToken(request);
-
-        return ResponseEntity.ok().body(token);
-    }
-
     @Data
-    static class CreateUserRequest{
+    static class CreateUserRequest {
         @NotNull
+        @Email
         private String email;
         @NotNull
         private String password;
     }
 
     @Data
-    static class DeleteUserRequest{
+    static class DeleteUserRequest {
         @NotNull
+        @Email
         private String email;
         @NotNull
         private String password;
