0a0deeb22ea4c853eedbf62570b620b3adb7967c
Jinsu <wlstncjs1234@naver.com>
2023-04-07 10:14:18 +0900

    jwt 토큰에서 User 정보 가져오기 (#18)

--- src/main/java/com/example/beside/common/config/InterCeptorConfig.java
+++ src/main/java/com/example/beside/common/config/InterCeptorConfig.java
@@ -1,18 +1,23 @@
 package com.example.beside.common.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.beside.common.jwt.JwtInterceptor;
+import com.example.beside.service.UserService;
 
 @Configuration
 public class InterCeptorConfig implements WebMvcConfigurer {
 
+    @Autowired
+    UserService userService;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // JWT 토큰 검증을 적용할 URL 패턴 지정
-        registry.addInterceptor(new JwtInterceptor())
+        registry.addInterceptor(new JwtInterceptor(userService))
                 .addPathPatterns("/api/users/v1/users");
     }
 
