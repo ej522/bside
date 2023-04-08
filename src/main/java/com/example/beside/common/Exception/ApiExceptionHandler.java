45998695ae9f774d6532020744e3db3968915733
jinsujj <wlstncjs1234@naver.com>
2023-04-08 10:49:40 +0900

    test 코드 작성

--- src/main/java/com/example/beside/common/Exception/ApiExceptionHandler.java
+++ src/main/java/com/example/beside/common/Exception/ApiExceptionHandler.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
