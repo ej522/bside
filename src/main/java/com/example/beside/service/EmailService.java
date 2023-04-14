26e1343f4a41d162c7d0197bd70d220c7c611409
Jinsu <wlstncjs1234@naver.com>
2023-04-14 15:45:35 +0900

    Moim 83 moil email css (#31)
    * 이메일 양식 첫 수정
    
    * 이메일 인증 CSS 변경

--- src/main/java/com/example/beside/service/EmailService.java
+++ src/main/java/com/example/beside/service/EmailService.java
@@ -40,13 +40,55 @@ public class EmailService {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        String htmlMsg = "<html><body>" +
-                "<h1>MOIM 서비스 이메일 인증</h1> <br/>" +
-                "<p>아래 인증번호를 MOIM 화면에 입력해주세요.</p> <br/>" +
-                "<div align='center' style='border:1px solid black; width:400px; font-family:verdana'>" +
-                "<h3 style='color:blue;'>Email 인증 코드입니다.</h3>" +
-                "<div style='font-size:130%'>CODE : <strong>" + verificationCode + "</strong></div>" +
-                "</div></body></html>";
+        String htmlMsg = " <div style='background-color: #F6F7FB; \n" +
+                "                padding-top:80px;\n" +
+                "                width:750px;\n" +
+                "                height:400px;\n" +
+                "                padding-left:60px;'> \n" +
+                " <h1 style='color: #1B1D1F;\n" +
+                "                    font-style: Pretendard; \n" +
+                "                    font-size:40px;\n" +
+                "                    line-height:22px;\n" +
+                "                    padding-bottom: 19px; \n" +
+                "                    font-weight: 700; '>\n" +
+                " MOIM 서비스 이메일 인증</h1>\n" +
+                " <div style='color: #626870; \n" +
+                "                    font-style:Pretendard;\n" +
+                "                    font-size:28px; \n" +
+                "                    line-height:46px;\n" +
+                "                    padding-bottom: 36px; \n" +
+                "                    font-weight: 500;'>\n" +
+                " 아래 인증번호를 MOIM 화면에 입력해주세요 </div>\n" +
+                " <div style='border:1px solid white;\n" +
+                "                    background-color: white;\n" +
+                "                    display: flex; \n" +
+                "                    flex-direction: column;\n" +
+                "                    justify-content: center;\n" +
+                "                    align-items: center; \n" +
+                "                    font-size: 14px;\n" +
+                "                    height: 191px; \n" +
+                "                    width: 690px;'>\n" +
+                " <div style='padding:54px 0;'>\n" +
+                " <div style='color: #626870;\n" +
+                "                            font-style: Pretendard; \n" +
+                "                            font-weight: 500;\n" +
+                "                            font-size:22px;\n" +
+                "                            line-height:20px;\n" +
+                "                            justify-content: center;\n" +
+                "                            padding-bottom:15px;\n" +
+                "                '>EMAIL 인증 코드 입니다</div>\n" +
+                " <div>\n" +
+                " <strong style='color: #8673FF;\n" +
+                "                                font-style: Pretendard;     \n" +
+                "                                font-size: 48px;\n" +
+                "                                font-weight: 700;\n" +
+                "                                line-height:48px;\n" +
+                "                                '>\n" + verificationCode +
+                " </strong>\n" +
+                " </div>\n" +
+                " </div>\n" +
+                " </div>\n" +
+                " </div>\n";
 
         helper.setFrom("admin@moim.life");
         helper.setTo(to);
