86ba3b747bbcc77c017a4586dc712c4407d13e8a
jinsujj <wlstncjs1234@naver.com>
2023-05-05 21:59:42 +0900

    Swagger Response 에 보일 DTO 가공

--- src/main/java/com/example/beside/dto/FriendDto.java
+++ src/main/java/com/example/beside/dto/FriendDto.java
@@ -1,8 +1,10 @@
 package com.example.beside.dto;
 
-
 import com.example.beside.domain.Friend;
 import com.example.beside.domain.User;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,14 +13,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class FriendDto {
-    public Long id;
-    public Long user_id;
+    @NotEmpty
+    @Schema(description = "처음 만난 모임 ID", example = "7979")
+    public Long first_moim_id;
+
+    @NotEmpty
+    @Schema(description = "친구 ID", example = "232")
     public Long friend_id;
+
+    @NotEmpty
+    @Schema(description = "친구 이름", example = "다람쥐")
     public String friend_name;
 
     public FriendDto(Friend friend, User user) {
-        this.id = friend.getId();
-        this.user_id = friend.getUser().getId();
+        this.first_moim_id = friend.getFirst_moim_id();
         this.friend_id = friend.getMember_id();
         friend_name = user.getName();
 
--- src/main/java/com/example/beside/dto/UserDto.java
+++ src/main/java/com/example/beside/dto/UserDto.java
@@ -2,6 +2,7 @@ package com.example.beside.dto;
 
 import com.example.beside.domain.User;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,16 +14,27 @@ import lombok.NoArgsConstructor;
 public class UserDto {
 
     @NotEmpty
+    @Schema(description = "사용자 ID", example = "1")
     private Long id;
+
     @NotEmpty
+    @Schema(description = "비밀번호", example = "password")
     private String password;
+
     @NotEmpty
+    @Schema(description = "이메일", example = "owl@moim.life")
     private String email;
+
     @NotEmpty
+    @Schema(description = "이름", example = "부엉이")
     private String name;
+
     @NotEmpty
+    @Schema(description = "프로필 이미지", example = "https://moim.life/profile/green.jpg")
     private String profile_image;
+
     @NotEmpty
+    @Schema(description = "소셜 타입", example = "MOIM")
     private String social_type;
 
     public UserDto(User user) {
