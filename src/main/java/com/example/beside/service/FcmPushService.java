2d8b37876e6fbfd345629656e4c9dabc04c00962
Jinsu <wlstncjs1234@naver.com>
2023-05-31 13:13:25 +0900

    fcm push 알람 기능 구현 (#71)
    * fcm push 알람 기능 구현
    
    * buld gradle add
    
    * github action 변경
    
    * github action 수정
    
    * test code 수정 TODO 로 남김

---
+++ src/main/java/com/example/beside/service/FcmPushService.java
@@ -0,0 +1,22 @@
+package com.example.beside.service;
+
+import org.springframework.stereotype.Service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+
+@Service
+public class FcmPushService {
+
+    public void sendFcmPushNotification(String fcmToken, String title, String body) throws FirebaseMessagingException {
+        Message message = Message.builder()
+                .setNotification(Notification.builder().setTitle(title).setBody(body).build())
+                .setToken(fcmToken)
+                .build();
+
+        String response = FirebaseMessaging.getInstance().send(message);
+        System.out.println("Successfully sent message: " + response);
+    }
+}
