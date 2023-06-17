e695ab01332561e6a40ff67d1c0c8ce3206e1d3c
jinsujj <wlstncjs1234@naver.com>
2023-06-17 13:06:46 +0900

    불필요한 Swagger 서버 제거

--- src/main/java/com/example/beside/common/config/SwaggerConfig.java
+++ src/main/java/com/example/beside/common/config/SwaggerConfig.java
@@ -25,9 +25,6 @@ public class SwaggerConfig {
         @Bean
         public OpenAPI customOpenAPI() {
                 return new OpenAPI()
-                                .servers(Arrays.asList(
-                                                new Server().url("https://moim.life/").description("Production server"),
-                                                new Server().url("http://localhost:8081").description("local server")))
                                 .info(new Info().title("b-side")
                                                 .description("API documentation using springdoc-openapi and OpenAPI 3.0")
                                                 .version("1.0.0")
