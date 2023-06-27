89484dc81427d5a105efc58283200b5deefc6922
jinsujj <wlstncjs1234@naver.com>
2023-06-27 22:42:16 +0900

    dto 위치 수정

--- src/main/java/com/example/beside/dto/MoimMemberDto.java
+++ src/main/java/com/example/beside/dto/MoimMemberDto.java
@@ -2,7 +2,6 @@ package com.example.beside.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
