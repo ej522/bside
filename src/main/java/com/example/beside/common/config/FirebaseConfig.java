a12a408ca62d455240550989b42905da6683f806
jinsujj <wlstncjs1234@naver.com>
2023-06-28 11:18:04 +0900

    파이어 베이스 원인 분석

--- src/main/java/com/example/beside/common/config/FirebaseConfig.java
+++ src/main/java/com/example/beside/common/config/FirebaseConfig.java
@@ -4,22 +4,28 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 
 @Configuration
 public class FirebaseConfig {
+    private static final Logger logger = LoggerFactory.getLogger(FirebaseConfig.class);
+
     @PostConstruct
-    public void init(){
-        try{
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
-            FirebaseOptions options = new FirebaseOptions.Builder()
+    public void init() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+            FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
+
             FirebaseApp.initializeApp(options);
-        }catch (Exception e){
+        } catch (Exception e) {
+            logger.error("firbase init error: ", e.getStackTrace());
             e.printStackTrace();
         }
     }
