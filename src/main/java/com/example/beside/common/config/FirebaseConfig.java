f4483843d9048fb380f21721386796e6a070a554
ej522 <jsej07@gmail.com>
2023-07-13 00:48:11 +0900

    Fcm 에러 방지 코드 추가

--- src/main/java/com/example/beside/common/config/FirebaseConfig.java
+++ src/main/java/com/example/beside/common/config/FirebaseConfig.java
@@ -7,18 +7,28 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
+import java.util.List;
 
 @Configuration
 public class FirebaseConfig {
     @PostConstruct
     public void init(){
         try{
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/serviceAccountKey.json");
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-            FirebaseApp.initializeApp(options);
+            FirebaseApp firebaseApp = null;
+            List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
+            if(firebaseApps != null && !firebaseApps.isEmpty()) {
+                for(FirebaseApp app : firebaseApps) {
+                    if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
+                        firebaseApp = app;
+                }
+            } else {
+                FileInputStream serviceAccount =
+                        new FileInputStream("src/main/resources/serviceAccountKey.json");
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+                FirebaseApp.initializeApp(options);
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
