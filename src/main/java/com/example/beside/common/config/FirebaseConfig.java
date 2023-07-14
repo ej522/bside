2fd428ec508b46e264463b43f24eacc42a742012
ej522 <jsej07@gmail.com>
2023-07-14 23:51:31 +0900

    fcm 초기화 코드 로그추가

--- src/main/java/com/example/beside/common/config/FirebaseConfig.java
+++ src/main/java/com/example/beside/common/config/FirebaseConfig.java
@@ -4,6 +4,8 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
@@ -11,26 +13,34 @@ import java.util.List;
 
 @Configuration
 public class FirebaseConfig {
+    private final Logger log = LoggerFactory.getLogger(FirebaseConfig.class);
 
     @PostConstruct
     public void init(){
+
         try{
+            log.info("try문");
             FirebaseApp firebaseApp = null;
             List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
             if(firebaseApps != null && !firebaseApps.isEmpty()) {
+                log.info("not empty");
                 for(FirebaseApp app : firebaseApps) {
                     if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
                         firebaseApp = app;
                 }
             } else {
+                log.info("empty");
                 FileInputStream serviceAccount =
                         new FileInputStream("src/main/resources/serviceAccountKey.json");
                 FirebaseOptions options = new FirebaseOptions.Builder()
                         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                         .build();
 
-                if(firebaseApps.isEmpty())
+                if(firebaseApps.isEmpty()){
+                    log.info("=======init starttttt=======");
                     firebaseApp = FirebaseApp.initializeApp(options);
+                    log.info("=======init enddddd=======");
+                }
 
             }
         }catch (Exception e){
